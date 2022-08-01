099f012b6ccae4b61ae2cd89f0ce9c860cfac8a2
reeturn01HPC <test@email.com>
2022-08-01 22:58:17 +0200

    Created abstract class Transaction

--- Sistem-za-bankarsko-rabotenje/src/main/java/classes/Transaction.java
+++ Sistem-za-bankarsko-rabotenje/src/main/java/classes/Transaction.java
@@ -1,14 +1,33 @@
 package classes;
 
-import exceptions.NotImplementedException;
-
 public abstract class Transaction {
 
+    private long formId;
+    private long toId;
+    private String description;
+    private String amount;
+
+    Transaction(long formId, long toId, String description, String amount){
+
+        this.formId = formId;
+        this.toId = toId;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public long getFormId() {
+        return formId;
+    }
+
+    public long getToId() {
+        return toId;
+    }
+
     public String getAmount() {
-        throw new NotImplementedException();
+        return amount;
     }
 
     public String getDescription() {
-        throw new NotImplementedException();
+        return description;
     }
 }
