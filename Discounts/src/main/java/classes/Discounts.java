3810870f300dffd7995e0c9bfe6a713a3a12a6ed
reeturn01HPC <test@email.com>
2022-08-14 21:04:45 +0200

    Created project Discounts and PROBLEM_DESCRIPTION.md

---
+++ Discounts/src/main/java/classes/Discounts.java
@@ -0,0 +1,18 @@
+package classes;
+
+import java.io.InputStream;
+import java.util.List;
+
+public class Discounts {
+    public int readStores(InputStream stream) {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Store> byAverageDiscount() {
+        throw new UnsupportedOperationException();
+    }
+
+    public List<Store> byTotalDiscount() {
+        throw new UnsupportedOperationException();
+    }
+}
