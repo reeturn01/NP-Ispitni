0945919be060aab3d20352829b5cf95376505ad8
reeturn01HPC <test@email.com>
2022-08-16 00:03:44 +0200

    Finished project Laboratoriski-vezbi

--- Laboratoriski-vezbi/src/main/java/classes/LabExercises.java
+++ Laboratoriski-vezbi/src/main/java/classes/LabExercises.java
@@ -1,20 +1,46 @@
 package classes;
 
-import java.util.Map;
+import java.util.*;
+
+import static java.util.Comparator.*;
+import static java.util.stream.Collectors.*;
 
 public class LabExercises {
+    public static final Comparator<Student> STUDENT_COMPARATOR_ASCENDING = comparingDouble(Student::averagePoints).thenComparing(Student::getIndex);
+    private static final Comparator<Student> STUDENT_COMPARATOR_DESCENDING = comparingDouble(Student::averagePoints).reversed().thenComparing(Student::getIndex, reverseOrder());
+    private final List<Student> students;
+
+    public LabExercises() {
+        this.students = new ArrayList<>();
+    }
+
     public void addStudent(Student student) {
-
+        students.add(student);
     }
 
-    public void printByAveragePoints(boolean b, int i) {
+    public void printByAveragePoints(boolean ascending, int n) {
+        
+        students.stream()
+                .sorted(ascending ? STUDENT_COMPARATOR_ASCENDING : STUDENT_COMPARATOR_DESCENDING)
+                .limit(n)
+                .forEachOrdered(System.out::println);
     }
 
-    public Iterable<Object> failedStudents() {
-        return null;
+    public List<Student> failedStudents() {
+        return students.stream()
+                .filter(Student::hasFailed)
+                .sorted(comparingInt(Student::getIndex).thenComparingDouble(Student::averagePoints))
+                .collect(toList());
     }
 
-    public Map<Object, Object> getStatisticsByYear() {
-        return null;
+    public Map<Integer, Double> getStatisticsByYear() {
+        return students.stream()
+                .filter(student -> !student.hasFailed())
+                .collect(groupingBy(Student::getStudentYear, TreeMap::new, collectingAndThen(
+                        toList(), list -> list.stream()
+                                .mapToDouble(Student::averagePoints)
+                                .average()
+                                .orElse(0.0)
+                )));
     }
 }
--- Laboratoriski-vezbi/src/main/java/classes/Student.java
+++ Laboratoriski-vezbi/src/main/java/classes/Student.java
@@ -1,9 +1,61 @@
 package classes;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Student {
-    public Student(String index, List<Integer> points) {
+    public static final int MINIMUM_NUMBER_OF_APPEARANCES_TO_NOT_FAIL = 8;
+    private final int index;
+    private final int studentYear;
+    private final List<Integer> points;
 
+    private Student() {
+        throw new RuntimeException();
+    }
+
+    public Student(String index, List<Integer> points) {
+        if (index.chars().allMatch(Character::isDigit) && points.size()<=10) {
+            this.index = Integer.parseInt(index);
+            this.studentYear = 10-Character.digit(index.charAt(1), 10);
+            this.points = points;
+        }
+        else {
+            throw new RuntimeException();
+        }
+    }
+    public double averagePoints(){
+        return points.stream()
+                .mapToInt(Integer::intValue)
+                .sum() / 10.0;
+    }
+    public int getIndex() {
+        return index;
+    }
+
+    public int getStudentYear() {
+        return studentYear;
+    }
+
+    public boolean hasFailed(){
+        return points.size()< MINIMUM_NUMBER_OF_APPEARANCES_TO_NOT_FAIL;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return index == student.index;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(index);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %s %.2f", index, hasFailed() ? "NO": "YES", averagePoints());
     }
 }
