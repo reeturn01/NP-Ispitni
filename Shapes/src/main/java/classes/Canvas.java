00a5809e765737b133c78773a4532a8e47fd3e60
reeturn01HPC <test@email.com>
2022-08-17 00:15:58 +0200

    Created project Shapes, PROBLEM_DESCRIPTION.md and IntegrationTest with Test Data

---
+++ Shapes/src/main/java/classes/Canvas.java
@@ -0,0 +1,26 @@
+package classes;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+
+public class Canvas {
+    public void readShapes(InputStream in) {
+        throw new RuntimeException();
+    }
+
+    public void printAllShapes(PrintStream out) {
+        throw new RuntimeException();
+    }
+
+    public void scaleShapes(String s, double v) {
+        throw new RuntimeException();
+    }
+
+    public void printByUserId(PrintStream out) {
+        throw new RuntimeException();
+    }
+
+    public void statistics(PrintStream out) {
+        throw new RuntimeException();
+    }
+}
